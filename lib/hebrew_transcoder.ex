ae4e3bb8a81024051bc21bd16733668cf13be73f
Jack Weinbender <jack.weinbender@gmail.com>
2016-05-08 23:28:38 -0500

    Added transcode function; better tests; more char_types in sort list

--- lib/hebrew_transcoder.ex
+++ lib/hebrew_transcoder.ex
@@ -17,23 +17,55 @@ defmodule HebrewTranscoder do
     |> Enum.reduce([],
       fn(char, acc)->
         if Regex.match?(~r/cons/, Map.fetch!(char, :char_type)) do
+          # Matches that contain `cons` in their `char_type` are
+          # given a new sub-list at the head of the main list
+          # additional vowels, etc. will be added to the sub-list
           [[char] | acc]
         else
+          # Reassigns the head of the list (i.e. the most recent syllable,
+          # see above) to `head` and prepends the new char to the syllable.
+          # Finally, it replaces the old head of the list with the new one
           [head | tail] = acc
           syllable = [char | head]
           List.replace_at(acc, 0, syllable)
         end
       end)
     |> Enum.map(
+      # Sorts each syllable by a consistent encoding order following
+      # the basic pattern:
+      #   1) Consonant
+      #   2) Dagesh [Encoded in the map]
+      #   3) Vowels
+      #   4) Cantilation Marks
+      #   5) everything else
       fn(syl)-> Enum.sort(syl, fn(a, b)->
-          order = ["cons", "vow", "cant"]
+          order = [
+            "cons",
+            "cons+dag",
+            "cons+vow",
+            "vow",
+            "cant",
+            "roman",
+            "other"
+          ]
           index_a = Enum.find_index(order, fn(x)-> x == a.char_type end)
           index_b = Enum.find_index(order, fn(x)-> x == b.char_type end)
+
           index_a < index_b
         end)
       end)
     |> List.flatten()
     |> Enum.map(fn(char)-> char.hebrew end)
     |> Enum.join()
+  end
+  def split_words(string) do
+    String.split(string, " ")
+  end
+  def transcode(match) do
+    match
+    |> split_words()
+    |> Enum.map(&syllabify/1)
+    |> Enum.reverse()
+    |> Enum.join(" ")
   end
 end
--- test/hebrew_transcoder_test.exs
+++ test/hebrew_transcoder_test.exs
@@ -19,5 +19,20 @@ defmodule HebrewTranscoderTest do
 
     assert test == expected
   end
+  test "fn `split_words` splits string on space'" do
+    input = ~s(aY"m;v.Bi Hl'a/ yt;yai)
+    expected = ["aY\"m;v.Bi", "Hl'a/", "yt;yai"]
 
+    test = HebrewTranscoder.split_words(input)
+
+    assert test == expected
+  end
+  test "fn `transcode` Splits, transcodes, and reassembles Hebrew phrases" do
+    input = ~s(aY"m;v.Bi Hl'a/ yt;yai)
+    expected = "אִיתַי אֱלָהּ בִּשְׁמַיָּא"
+
+    test = HebrewTranscoder.transcode(input)
+
+    assert test == expected
+  end
 end
